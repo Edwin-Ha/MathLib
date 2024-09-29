242f462976682df6ce8277c695006e6722b880a6
Edwin Ha <180835459+Edwin-Ha@users.noreply.github.com>
2024-09-29 11:58:47 +0200

    Add the MathLib with 2 useful functions LCM and GDC

---
+++ MathLib/MathLib.Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MathLib.Sample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var n1 = 17640;
+        var n2 = 411600;
+
+        var gcd = MathLib.GCD(n1, n2);
+        var lcm = MathLib.LCM(n1, n2);
+
+        Console.WriteLine($"LCM({n1}, {n2}) = {lcm}");
+        Console.WriteLine($"GCD({n1}, {n2}) = {gcd}");
+
+    }
+}
---
+++ MathLib/MathLib/MathLib.cs
@@ -0,0 +1,23 @@
+﻿namespace MathLib;
+
+public class MathLib
+{
+    public static long GCD(long a, long b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static long LCM(long a, long b)
+    {
+        return Math.Abs(a * b) / GCD(a, b);
+    }
+}
